9e51608a43a2c15d7ebdd1b5219644ec1d6f8f26
John Otieno <jomakonyango@gmail.com>
2024-06-08 11:07:59 +0300

    numbr of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,20 +4,27 @@
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
     """
     # Set custom User-Agent
     headers = {'User-Agent': 'my-app/0.0.1'}
 
     # Send a GET request to the Reddit API
-    response = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json', headers=headers, allow_redirects=False)
+    response = requests.get(
+        'https://www.reddit.com/r/{}/about.json'.format(subreddit),
+        headers=headers,
+        allow_redirects=False
+    )
 
     # Check if the subreddit is valid
     if response.status_code != 200:
         return 0
 
-    # Parse the response and get the number of subscribers
+    # Get the number of subscribers
     data = response.json()
     return data['data']['subscribers']
+
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,4 @@
+0x16. API advanced
+
+This readme expalins on how :
+1. Using an a function to return number of subscribers on a Rededit
