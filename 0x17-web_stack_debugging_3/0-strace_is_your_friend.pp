6c3cf0c15310e8cd7a40caa991020476c5a4e3d9
John Otieno <jomakonyango@gmail.com>
2024-06-11 21:41:44 +0300

    puppet code

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,13 @@
+# Puppet manifest to fix the issue identified with strace
+# Replace bad "phpp" extension to "php" in "wp-settings.php".
+exec { 'fix_wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path => '/usr/local/bin/:/bin/',
+}
+
+# Notify a service restart if necessary
+service { 'apache2':
+  ensure => running,
+  enable => true,
+  require => Exec ['fix_wordpress'],
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,2 @@
+0x17. Web stack debugging #3
+This is puppet code to help solve error 500 on Apache
