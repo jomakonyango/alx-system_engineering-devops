267b4731e742470fafcafa1a6874bd9899dda9b9
John Otieno <jomakonyango@gmail.com>
2024-06-11 21:48:16 +0300

    puppet code

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,12 +2,12 @@
 # Replace bad "phpp" extension to "php" in "wp-settings.php".
 exec { 'fix_wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path => '/usr/local/bin/:/bin/',
+  path    => '/usr/local/bin/:/bin/',
 }
 
 # Notify a service restart if necessary
 service { 'apache2':
-  ensure => running,
-  enable => true,
+  ensure  => running,
+  enable  => true,
   require => Exec ['fix_wordpress'],
 }
