c5b57adfd8cb390da72a9370581534a95e1dda9a
John Otieno <jomakonyango@gmail.com>
2024-08-23 16:48:06 +0300

    Redit subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module to query the Reddit API and return the number of subscribers
+for a given subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit. If the subreddit is invalid, returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'my-app/0.0.1'}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception:
+        return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,15 @@
+0. How many subs?
+# Reddit API Subscriber Count
+
+This project contains a Python script that queries the Reddit API to return the number of subscribers for a given subreddit. If an invalid subreddit is given, the function returns 0.
+
+## Requirements
+
+- Python 3.4.3
+- Requests module
+
+## Usage
+
+```bash
+./0-main.py <subreddit>
+
