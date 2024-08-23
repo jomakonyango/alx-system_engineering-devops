05310a54718e3fb74b48f86d076c12f806e8950a
John Otieno <jomakonyango@gmail.com>
2024-08-23 17:02:21 +0300

    Redit subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,6 +18,7 @@ def number_of_subscribers(subreddit):
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
+            print(f"Data: {data}")
             return data['data']['subscribers']
         else:
             return 0
